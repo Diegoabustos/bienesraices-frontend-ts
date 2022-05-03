9cff1105f42e85a91fb4cd01c37213a5ac783c52
Diego <diegoabustos24@gmail.com>
2022-05-02 21:39:47 -0500

    css to nav

--- src/pages/nosotros.tsx
+++ src/pages/nosotros.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Layout } from "../components/layout";
 
-export const Nosotros = () => {
+const Nosotros = () => {
     return (
         <Layout>
             <h1>Nosotros</h1>
         </Layout>
     )
 };
+
+export default Nosotros;
--- src/pages/propiedades.tsx
+++ src/pages/propiedades.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Layout } from "../components/layout";
 
-export const Propiedades = () => {
+const Propiedades = () => {
     return (
         <Layout>
             <h1>Propiedades</h1>
         </Layout>
     )
 };
+
+export default Propiedades;
